e5ecb6437f82bfc9a64b5e8f6c64ec7b09e755b6
Lucas Fogaça <lucasfgc00@gmail.com>
2015-09-10 21:19:12 -0300

    Update Final
    The gambiarra works!

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -20,6 +20,12 @@ public class ContaCorrente {
 	}
 	
 	
+	public ContaCorrente(int numeroConta) {
+		// TODO Auto-generated constructor stub
+		this.numero=numeroConta;
+	}
+
+
 	public double getLimite() {
 		return limite;
 	}
@@ -42,9 +48,6 @@ public class ContaCorrente {
 	
 	
 	public void lerContaCorrente(){
-		
-		System.out.println("Informe o titular: ");
-		titular.lerCliente();
 		
 		System.out.println("Informe o numero da conta: ");
 		this.setNumero(tc.nextInt());
@@ -85,11 +88,15 @@ public class ContaCorrente {
 
 	public void mostrarContaCorrente(){
 		System.out.println("Informações da Conta: ");
+		System.out.println("Agencia");
 		System.out.println(this.getAgencia());
+		System.out.println("Numero");
 		System.out.println(this.getNumero());
+		System.out.println("Saldo");
 		System.out.println(this.getSaldo());
+		System.out.println("Senha");
 		System.out.println(this.getSenha());
-		System.out.println(this.getValorDeposito());
+		//System.out.println(this.getValorDeposito());
 	}
 	
 	public Cliente getTitular() {
@@ -132,14 +139,33 @@ public class ContaCorrente {
 		return true;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + numero;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContaCorrente other = (ContaCorrente) obj;
+		if (numero != other.numero)
+			return false;
+		return true;
+	}
+
+
 	public boolean sacar( double valor){
-		if(this.getSaldo()+this.getLimite()<valor){
 			setSaldo(this.getSaldo()-valor);
 			return true;
-		}
-		else{
-			return false;
-		}
 	}
 	
 	public boolean tranferencia(ContaCorrente destino, double valor){
--- src/Programa.java
+++ src/Programa.java
@@ -6,8 +6,7 @@ public class Programa {
 	
 	public static void main(String[] args) {
 		
-		int opc;
-		int count = 0;
+		int opc=0;
 		Scanner tc = new Scanner(System.in);
 		
 		
@@ -15,9 +14,10 @@ public class Programa {
 		
 		
 		
-		System.out.println("Digite:1- cadastrar conta.\n 2- Efetuar Deposito:\n 3- Efetuar Saque: \n 4- Efetuar Tranferencia:\n");
-		opc=tc.nextInt();
 		do{
+			System.out.println("Digite:\n 1- cadastrar conta.\n 2- Efetuar Deposito:\n 3- Efetuar Saque: \n 4- Efetuar Tranferencia:\n 5-Mostrar as contas:\n 6-Sair");
+			opc=tc.nextInt();
+			
 		switch(opc){
 		
 		case 1:{
@@ -25,8 +25,9 @@ public class Programa {
 			Cliente c1 = new Cliente(tc.next());
 			c1.lerCliente();
 			ContaCorrente conta = new ContaCorrente(c1);
+			conta.lerContaCorrente();;	
 			if(!contas.contains(conta)){
-				conta.lerContaCorrente();;	
+				
 				contas.add(conta);
 			}
 			break;
@@ -38,12 +39,62 @@ public class Programa {
 			valor=tc.nextDouble();
 			System.out.println("Informe o numero da conta destino");
 			numeroConta=tc.nextInt();
-			contas.contains(contas.indexOf(numeroConta));
-			
+			ContaCorrente c= new ContaCorrente(numeroConta);
+			int i= contas.indexOf(c);
+			c=contas.get(i);
+			c.depositar(valor);
+			break;
 		}
+		case 3:{
+			double valor;
+			int numeroConta;
+			System.out.println("Digite um valor");
+			valor=tc.nextDouble();
+			System.out.println("Informe o numero");
+			numeroConta=tc.nextInt();
+			ContaCorrente c= new ContaCorrente(numeroConta);
+			int i= contas.indexOf(c);
+			c=contas.get(i);
+			c.sacar(valor);
+			break;
 		}
 		
-	}while(opc!=0);
+		case 4:{
+			double valor;
+			int numeroConta;
+			int numeroConta2;
+			System.out.println("Digite um valor ");
+			valor=tc.nextDouble();
+			System.out.println("Informe o numero da conta de origem");
+			numeroConta=tc.nextInt();
+			ContaCorrente c= new ContaCorrente(numeroConta);
+			int i= contas.indexOf(c);
+			c=contas.get(i);
+			c.sacar(valor);
+	
+			System.out.println("Informe o numero da conta Destino");
+			numeroConta2=tc.nextInt();
+			ContaCorrente c2= new ContaCorrente(numeroConta2);
+			int i2= contas.indexOf(c2);
+			c2=contas.get(i2);
+			c2.depositar(valor);
+			break;
+			
+		}
+		
+		case 5:{
+			int numeroConta;
+			System.out.println("Informe o numro da conta");
+			numeroConta=tc.nextInt();
+			ContaCorrente c = new ContaCorrente(numeroConta);
+			int posicao = contas.indexOf(c);
+			c=contas.get(posicao);
+			c.mostrarContaCorrente();
+		break;
+		}
+		}
+	}while(opc!=6);
 
 }
 }
+
